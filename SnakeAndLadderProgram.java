089f58c38fcfee051f45bccd44b8da5e24813e9c
Deepika1701 <deepikakumari1701.com>
2021-10-19 22:01:04 +0530

    Added: Welcome to Snake and ladder game

---
+++ SnakeAndLadderProgram.java
@@ -0,0 +1,9 @@
+package com.bridgelabs.SnakeAndLadder;
+
+public class SnakeAndLadderProgram {
+
+    public static void main(String args[]){
+
+        System.out.println("Welcome to snake and ladder game");
+    }
+}
