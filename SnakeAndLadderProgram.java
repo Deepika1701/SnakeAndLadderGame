41d393e3481bec3ed7e19821c3471b1d6fbced82
Deepika1701 <deepikakumari1701.com>
2021-10-19 22:50:29 +0530

    Added: Player rolls the die to get number between 1 to 6

--- SnakeAndLadderProgram.java
+++ SnakeAndLadderProgram.java
@@ -3,7 +3,13 @@ package com.bridgelabs.SnakeAndLadder;
 public class SnakeAndLadderProgram {
 
     public static void main(String args[]){
-        int START = 0;
-        System.out.println("Single Player at start:"+START);
+
+        double empCheck = (Math.floor(Math.random() * 10) % 5) + 1;
+        double randomNum = Math.random();
+        double randomNum2 = randomNum * 10;
+        double randomNum3 = Math.floor(randomNum2);
+        double dieRoll = (randomNum3 % 5) +1;
+        System.out.println("PLayer get " +dieRoll+ " after rolling a die ");
+
     }
 }
